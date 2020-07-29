272265ae8d65d6e5a3dd15d1d053eb059740e1f8
Javi <you@example.com>
2020-07-29 15:13:00 -0500

    update

--- src/exhibitor/Exhibitor.js
+++ src/exhibitor/Exhibitor.js
@@ -7,6 +7,7 @@ import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
 import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player'
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
@@ -22,6 +23,8 @@ class Exhibitor extends Component {
         this.handleHideServices = this.handleHideServices.bind(this)
         this.handleShowInfo = this.handleShowInfo.bind(this)
         this.handleHideInfo = this.handleHideInfo.bind(this)
+        this.handleShowVideo = this.handleShowVideo.bind(this)
+        this.handleHideVideo = this.handleHideVideo.bind(this)
         this.state = {
             showGallery: false,
             showDocuments: false,
@@ -63,10 +66,19 @@ class Exhibitor extends Component {
         this.setState({ showInfo: false })
     }
 
+    handleShowVideo() {
+        this.setState({ showVideo: true })
+    }
+
+    handleHideVideo() {
+        this.setState({ showVideo: false })
+    }
+
     render() {
         let exhibitorData = {
-            video_iframe: "",
-            stand_type: "",
+            webpage: "https://hey-agency.com",
+            video_url: "https://www.youtube.com/watch?v=ysz5S6PUM-U",
+            stand_type: "silver", // gold,silver,bronze
             contact_info: {
                 description: "Para nosotros prestarte el mejor servicio es lo más importante\n" +
                     "\n" +
@@ -82,34 +94,20 @@ class Exhibitor extends Component {
             },
             services: [
                 {
-                    name: "Service 1",
                     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                     image: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FchatImage%20(1).png?alt=media&token=8b0577e2-72c8-4aa6-979c-3da53e25227f",
                 },
                 {
-                    name: "Service 2",
                     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                     image: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FchatImage%20(1).png?alt=media&token=8b0577e2-72c8-4aa6-979c-3da53e25227f",
                 },
                 {
-                    name: "Service 3",
                     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                     image: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FchatImage%20(1).png?alt=media&token=8b0577e2-72c8-4aa6-979c-3da53e25227f",
                 },
                 {
-                    name: "Service 4",
                     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
                     image: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FchatImage%20(1).png?alt=media&token=8b0577e2-72c8-4aa6-979c-3da53e25227f",
-                },
-            ],
-            documents: [
-                {
-                    name: "Nuestros productos",
-                    url: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FPDF-PRUEBAS.pdf?alt=media&token=afe56d5d-8d99-4ed3-a7e2-bc1e1a9719d6"
-                },
-                {
-                    name: "Nuestros servicios",
-                    url: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FPDF-PRUEBAS.pdf?alt=media&token=afe56d5d-8d99-4ed3-a7e2-bc1e1a9719d6"
                 },
             ],
             advisor: {
@@ -133,32 +131,33 @@ class Exhibitor extends Component {
             ],
             "social_networks": [
                 {
-                    "url": "https://facebook.com",
-                    "icon": "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2Fsocial%20icon.png?alt=media&token=eaa001e0-ea2d-444d-9070-6e6b6f6f4e22",
-                    "label": "Facebook"
+                    "url": "https://monoe.co",
+                    "network": "facebook"
                 },
                 {
-                    "url": "https://facebook.com",
-                    "icon": "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2Fsocial%20icon.png?alt=media&token=eaa001e0-ea2d-444d-9070-6e6b6f6f4e22",
-                    "label": "Facebook"
+                    "url": "https://monoe.co",
+                    "network": "instagram"
+                },
+                {
+                    "url": "https://monoe.co",
+                    "network": "twitter"
                 },
             ],
             // gold
-            stand_image: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FSTAND_ORO%20(1).png?alt=media&token=1f1e3de9-f75a-45f9-88f1-6b934ca3f4a4",
+            // stand_image: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FSTAND_ORO%20(1).png?alt=media&token=1f1e3de9-f75a-45f9-88f1-6b934ca3f4a4",
             // Bronce/Plata
-            // stand_image: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FSTAND_PLATA%20(1).png?alt=media&token=b6543477-a252-47de-858e-0427e95ff60d",
+            stand_image: "https://firebasestorage.googleapis.com/v0/b/hey-48c29.appspot.com/o/events%2FSTAND_PLATA%20(1).png?alt=media&token=b6543477-a252-47de-858e-0427e95ff60d",
         };
-
         return (
-            <div className="main-stand">
+            <div className={`main-stand main-stand-${exhibitorData.stand_type}`}>
                 <Link to="/exhibitors" className="main-stand-goback">
                     <img src="/exhibitors/icons/baseline_arrow_back_white_18dp.png" alt=""/>
                     Regresar
                 </Link>
                 <div className="main-stand-social-networks">
                     {exhibitorData.social_networks.map((item, index) => (
-                        <a href={item.url} className="main-stand-social-networks-item" key={index} target="_blank">
-                            <img src={item.icon} alt={item.label}/>
+                        <a href={item.url} className={`main-stand-social-networks-item ${item.network}`} key={index} target="_blank">
+                            <img src={`/exhibitors/icons/${item.network}.svg`} alt=""/>
                         </a>
                     ))}
                 </div>
@@ -187,12 +186,12 @@ class Exhibitor extends Component {
                         </div>
                         <div className="main-stand-navigation-item-label">CATÁLOGOS</div>
                     </div>
-                    <div className="main-stand-navigation-item">
+                    <a href={exhibitorData.webpage} className="main-stand-navigation-item" target="_blank">
                         <div className="main-stand-navigation-item-icon">
                             <img src="/exhibitors/icons/ic_public_24px.png" alt=""/>
                         </div>
                         <div className="main-stand-navigation-item-label">SITIO WEB</div>
-                    </div>
+                    </a>
                 </div>
                 <a className="main-stand-chat"
                    href={"https://api.whatsapp.com/send?phone=" + exhibitorData.advisor.phone_number} target="_blank">
@@ -205,6 +204,7 @@ class Exhibitor extends Component {
                 <div className="main-stand-module">
                     <div className="main-stand-module-wrap">
                         <img className="stand-image" src={exhibitorData.stand_image} alt=""/>
+                        <div className="main-stand-play-btn" onClick={this.handleShowVideo} />
                     </div>
                 </div>
                 <div className={`main-stand-gallery ${this.state.showGallery ? 'active' : ''}`}>
@@ -231,7 +231,7 @@ class Exhibitor extends Component {
                         </Swiper>
                     </div>
                 </div>
-                <div className={`main-stand-modal ${this.state.showDocuments ? 'active' : ''}`}>
+                {/*<div className={`main-stand-modal ${this.state.showDocuments ? 'active' : ''}`}>
                     <div className="main-stand-modal-overlay" onClick={this.handleHideDocuments} />
                     <div className={`main-stand-modal-container`}>
                         <div className="main-stand-modal-header">
@@ -241,18 +241,16 @@ class Exhibitor extends Component {
                             </div>
                         </div>
                         <div className="main-stand-modal-content">
-                            {exhibitorData.documents.map((doc, index) => (
-                                <div className="main-stand-documents-item" key={index}>
-                                    <div>{doc.name}</div>
-                                    <a href={doc.url} target="_blank" className="main-stand-documents-item-btn">
-                                        <img src="/exhibitors/icons/ic_dowload.png" alt=""/>
-                                        <span>Descargar</span>
-                                    </a>
-                                </div>
-                            ))}
+                            <div className="main-stand-documents-item" key={index}>
+                                <div>{doc.name}</div>
+                                <a href={doc.url} target="_blank" className="main-stand-documents-item-btn">
+                                    <img src="/exhibitors/icons/ic_dowload.png" alt=""/>
+                                    <span>Descargar</span>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div>*/}
                 <div className={`main-stand-modal ${this.state.showServices ? 'active' : ''}`}>
                     <div className="main-stand-modal-overlay" onClick={this.handleHideServices} />
                     <div className={`main-stand-modal-container`}>
@@ -269,7 +267,6 @@ class Exhibitor extends Component {
                                         <img src={service.image} alt=""/>
                                     </div>
                                     <div className="main-stand-services-item-info">
-                                        <div className="main-stand-services-item-info-title">{service.name}</div>
                                         <div>{service.description}</div>
                                     </div>
                                 </div>
@@ -299,6 +296,13 @@ class Exhibitor extends Component {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+
+                <div className={`main-stand-modal main-stand-modal-video ${this.state.showVideo ? 'active' : ''}`}>
+                    <div className="main-stand-modal-overlay" onClick={this.handleHideVideo} />
+                    <div className="main-stand-modal-video-wrap">
+                        <ReactPlayer url={exhibitorData.video_url} />
                     </div>
                 </div>
             </div>
